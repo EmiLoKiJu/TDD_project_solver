2daae8367de8911ae3721fc15a2d865d6ea5e94c
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-08-02 11:33:47 -0800

    Add tests for factorial and reverse methods

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,1 +1,27 @@
 require_relative '../solver.rb'
+
+RSpec.describe Solver do
+  let(:solver) { Solver.new }
+
+  context '#factorial' do
+    it 'should return the right value when given an integer greater than 1' do
+      expect(solver.factorial(3)).to eq(6)
+      expect(solver.factorial(8)).to eq(40320)
+    end
+
+    it 'should return 1 when the number given is 0' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+
+    it 'should return -1 when the number given is less than 0' do
+      expect(solver.factorial(-5)).to eq(-1)
+    end
+  end
+
+  context '#reverse' do
+    it 'should return the string in reverse' do
+      expect(solver.reverse('word')).to eq('drow')
+      expect(solver.reverse('aleluyah')).to eq('hayulela')
+    end
+  end
+end
